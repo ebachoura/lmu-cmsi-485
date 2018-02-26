76efa634ad0fa057819a9d41006c42adc0541e6d
ebachoura <edlobacro@gmail.com>
2018-02-26 15:37:53 -0800

    added progress on homework1

---
+++ homework2/MazeClause.py
@@ -0,0 +1,150 @@
+'''
+MazeClause.py
+Name: Edward Bachoura
+
+Specifies a Propositional Logic Clause formatted specifically
+for Grid Maze Pathfinding problems. Clauses are a disjunction of
+GridPropositions (2-tuples of (symbol, location)) mapped to
+their negated status in the sentence.
+'''
+import unittest
+
+class MazeClause:
+
+    def __init__ (self, props):
+        self.props = {}
+        self.valid = False
+        for prop in props:
+            if prop[0] in self.props and self.props[prop[0]] != prop[1]:
+                self.props, self.valid = {}, True
+                break
+            self.props[prop[0]] = prop[1]
+
+    def getProp (self, prop):
+        if prop in self.props:
+            return self.props[prop]
+        return None
+
+    def isValid (self):
+        return self.valid
+
+    def isEmpty (self):
+        return not self.valid and not self.props
+
+    def negate (self):
+        negatedSet = set()
+        if self.isEmpty() or self.isValid():
+            return negatedSet
+        for prop, value in self.props.items():
+            a = MazeClause([(prop, not value)])
+            negatedSet.add(a)
+        return negatedSet
+
+    def __eq__ (self, other):
+        return self.props == other.props and self.valid == other.valid
+
+    def __hash__ (self):
+        # Hashes an immutable set of the stored props for ease of
+        # lookup in a set
+        return hash(frozenset(self.props.items()))
+
+    def __str__ (self):
+        if self.valid:
+            return "Valid"
+        return str(self.props)
+
+    @staticmethod
+    def resolve (c1, c2):
+        results = set()
+
+        complementaryProposition = None
+        for prop in c1.props:
+            propValueInC2 = c2.getProp(prop)
+            if propValueInC2 == None:
+                continue
+            if not c1.props[prop] == propValueInC2:
+                complementaryProposition = prop
+                break
+        if not complementaryProposition:
+            return results
+
+        resolutionProps = []
+        for prop, value in c1.props.items():
+            if prop != complementaryProposition:
+                resolutionProps.append((prop, value))
+        for prop, value in c2.props.items():
+            if prop != complementaryProposition:
+                resolutionProps.append((prop, value))
+
+        newMazeClause = MazeClause(resolutionProps)
+        if not newMazeClause.isValid():
+            results.add(newMazeClause)
+        return results
+
+
+class MazeClauseTests(unittest.TestCase):
+    def test_mazeprops1(self):
+        mc = MazeClause([(("X", (1, 1)), True), (("X", (2, 1)), True), (("Y", (1, 2)), False)])
+        self.assertTrue(mc.getProp(("X", (1, 1))))
+        self.assertTrue(mc.getProp(("X", (2, 1))))
+        self.assertFalse(mc.getProp(("Y", (1, 2))))
+        self.assertTrue(mc.getProp(("X", (2, 2))) is None)
+        self.assertFalse(mc.isEmpty())
+
+    def test_mazeprops2(self):
+        mc = MazeClause([(("X", (1, 1)), True), (("X", (1, 1)), True)])
+        self.assertTrue(mc.getProp(("X", (1, 1))))
+        self.assertFalse(mc.isEmpty())
+
+    def test_mazeprops3(self):
+        mc = MazeClause([(("X", (1, 1)), True), (("Y", (2, 1)), True), (("X", (1, 1)), False)])
+        self.assertTrue(mc.isValid())
+        self.assertTrue(mc.getProp(("X", (1, 1))) is None)
+        self.assertFalse(mc.isEmpty())
+
+    def test_mazeprops4(self):
+        mc = MazeClause([])
+        self.assertFalse(mc.isValid())
+        self.assertTrue(mc.isEmpty())
+
+    def test_mazeprops5(self):
+        mc1 = MazeClause([(("X", (1, 1)), True)])
+        mc2 = MazeClause([(("X", (1, 1)), True)])
+        res = MazeClause.resolve(mc1, mc2)
+        self.assertEqual(len(res), 0)
+
+    def test_mazeprops6(self):
+        mc1 = MazeClause([(("X", (1, 1)), True)])
+        mc2 = MazeClause([(("X", (1, 1)), False)])
+        res = MazeClause.resolve(mc1, mc2)
+        self.assertEqual(len(res), 1)
+        self.assertTrue(MazeClause([]) in res)
+
+    def test_mazeprops7(self):
+        mc1 = MazeClause([(("X", (1, 1)), True), (("Y", (1, 1)), True)])
+        mc2 = MazeClause([(("X", (1, 1)), False), (("Y", (2, 2)), True)])
+        res = MazeClause.resolve(mc1, mc2)
+        self.assertEqual(len(res), 1)
+        self.assertTrue(MazeClause([(("Y", (1, 1)), True), (("Y", (2, 2)), True)]) in res)
+
+    def test_mazeprops8(self):
+        mc1 = MazeClause([(("X", (1, 1)), True), (("Y", (1, 1)), False)])
+        mc2 = MazeClause([(("X", (1, 1)), False), (("Y", (1, 1)), True)])
+        res = MazeClause.resolve(mc1, mc2)
+        self.assertEqual(len(res), 0)
+
+    def test_mazeprops9(self):
+        mc1 = MazeClause([(("X", (1, 1)), True), (("Y", (1, 1)), False), (("Z", (1, 1)), True)])
+        mc2 = MazeClause([(("X", (1, 1)), False), (("Y", (1, 1)), True), (("W", (1, 1)), False)])
+        res = MazeClause.resolve(mc1, mc2)
+        self.assertEqual(len(res), 0)
+
+    def test_mazeprops10(self):
+        mc1 = MazeClause([(("X", (1, 1)), True), (("Y", (1, 1)), False), (("Z", (1, 1)), True)])
+        mc2 = MazeClause([(("X", (1, 1)), False), (("Y", (1, 1)), False), (("W", (1, 1)), False)])
+        res = MazeClause.resolve(mc1, mc2)
+        self.assertEqual(len(res), 1)
+        self.assertTrue(MazeClause([(("Y", (1, 1)), False), (("Z", (1, 1)), True), (("W", (1, 1)), False)]) in res)
+
+if __name__ == "__main__":
+    unittest.main()
